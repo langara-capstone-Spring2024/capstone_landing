d46ce1f39e90ecb4274cf844c3cc51346a604730
laubana <45812484+laubana@users.noreply.github.com>
2024-02-25 11:04:42 -0800

    change router (#3)

---
+++ src/page/Home/Home.view.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./Home.module.css";
+
+const HomeView = (props) => {
+  const {} = props;
+
+  return <></>;
+};
+
+export default HomeView;
---
+++ src/page/Test/Test.view.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./Test.module.css";
+
+const TestView = (props) => {
+  const {} = props;
+
+  return <></>;
+};
+
+export default TestView;
