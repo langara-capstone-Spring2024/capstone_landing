68c0b897033a03f63cf8e71ddd3e2a38c700e2bc
yban <laubana@gmail.com>
2024-02-25 11:03:58 -0800

    change router

---
+++ src/page/Home/Home.container.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import HomeView from "./Home.view";
+
+const Home = () => {
+  const generatedProps = {};
+  return <HomeView {...generatedProps} />;
+};
+
+export default Home;
---
+++ src/page/Test/Test.container.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import TestView from "./Test.view";
+
+const Test = () => {
+  const generatedProps = {};
+  return <TestView {...generatedProps} />;
+};
+
+export default Test;
