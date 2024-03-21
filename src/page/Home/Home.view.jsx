61d729ba2183e12b21a1c9e1a16258ff2df2643b
hcheng40@mylangara.ca <>
2024-03-20 22:49:50 -0700

    add general layout

--- src/page/Home/Home.view.jsx
+++ src/page/Home/Home.view.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import styles from "./Home.module.css";
+import Header from "../../component/base/Header/Header";
+import Footer from "../../component/base/Footer/Footer";
+import TopSection from "../../component/base/TopSection/TopSection";
+import FeatureSection from "../../component/base/FeatureSection/FeatureSection";
+import TeamSection from "./../../component/base/TeamSection/TeamSection";
+import VideoSection from "../../component/base/VideoSection/VideoSection";
 
 const HomeView = (props) => {
   const {} = props;
 
-  return <>Home</>;
+  return (
+    <div>
+      <Header />
+      <div id="home">
+        <TopSection />
+      </div>
+      <div id="features">
+        <FeatureSection />
+      </div>
+      <VideoSection />
+      <div id="team">
+        <TeamSection />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default HomeView;
